2ad6850b5438781d08433f02507fb9251b0de527
naielv <109038805+naielv@users.noreply.github.com>
2024-09-01 10:23:29 +0200

    update fs.php

--- fs.php
+++ fs.php
@@ -16,9 +16,34 @@
     fclose($fi);
     fclose($fo);
   }
+  function dir_tree($dir_path) {
+    $rdi = new \RecursiveDirectoryIterator($dir_path);
+  
+    $rii = new \RecursiveIteratorIterator($rdi);
+  
+    $tree = [];
+  
+    foreach ($rii as $splFileInfo) {
+      $file_name = $splFileInfo->getFilename();
+  
+      // Skip hidden files and directories.
+      if ($file_name[0] === '.') {
+        continue;
+      }
+  
+      $path = $splFileInfo->isDir() ? array($file_name => array()) : array($file_name);
+  
+      for ($depth = $rii->getDepth() - 1; $depth >= 0; $depth--) {
+        $path = array($rii->getSubIterator($depth)->current()->getFilename() => $path);
+      }
+  
+      $tree = array_merge_recursive($tree, $path);
+    }
+    return $tree;
+  }
   if ($_GET["cmd"] == "list") {
     header('Content-Type: application/json');
 
-    echo json_encode(scandir($base));
+    echo json_encode(dir_tree($base));
   }
 ?>
