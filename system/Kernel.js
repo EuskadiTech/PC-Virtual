fc9baa4cb5067dd4eeaf7a9f32354d61095ea1b4
naielv <109038805+naielv@users.noreply.github.com>
2024-09-07 20:53:16 +0200

    update

--- system/Kernel.js
+++ system/Kernel.js
@@ -203,7 +203,7 @@ let Annapurna = {
     },
     Activation: {
         load_license: (license, callback_ok = () => { }, callback_fail = () => { }) => {
-            fetch("https://es01.naiel.fyi/license/" + license.toUpperCase()).then(res => res.json()).then(json => {
+            fetch("https://abe.tech.eus/license/" + license.toUpperCase()).then(res => res.json()).then(json => {
                 var url = json.fs_baseurl
                 localStorage.setItem("annapurna_fs_base", url)
                 FS_BASE = url;
